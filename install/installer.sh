b4cad431569b893f31ba82ffea27c4aff5913ed7
matsumiya8 <github@pyne.top>
2025-03-04 08:19:37 -0300

    add installer

---
+++ install/installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $HOME
+#sudo pacman -S --needed base-devel
+#git clone https://aur.archlinux.org/paru.git
+#cd paru
+#makepkg -si
+
+PKGLIST=$(curl -fsSL https://raw.githubusercontent.com/matsumiya8/dotfiles/refs/heads/main/install/packages.txt | paste -sd ' ')
+
+paru -S --skipreview --nosudoloop $PKGLIST
+
+chezmoi init https://github.com/matsumiya8/dotfiles.git
+chezmoi apply
+chsh -s /usr/bin/zsh
+systemctl --user enable --now pipewire-pulse.service
+sudo systemctl enable ly.service

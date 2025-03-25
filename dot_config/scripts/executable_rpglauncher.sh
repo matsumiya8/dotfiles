3542faed495ca8f99638584fbef1acd8d3908c16
matsumiya8 <github@pyne.top>
2025-03-24 23:58:12 -0300

    readd wayland flag when initial size bug is fixed

--- dot_config/scripts/executable_rpglauncher.sh
+++ dot_config/scripts/executable_rpglauncher.sh
@@ -9,15 +9,12 @@ selected_game=$(ls --quoting-style=literal $base_dir | walker -d)
 
 # Checking whether or not a choice was made, and finding where the index.html of the game is
 if [ -n "$selected_game" ]; then
-  game_dir=$(find "$base_dir/$selected_game" -type f -name "index.html" -exec dirname {} \;)  
+  game_dir=$(find "$base_dir/$selected_game" -type f -name "index.html" -exec dirname {} \;)
 else
   notify-send -t 2000 "Aborting..."
   exit
 fi
 
-# Switching to the gaming workspace
-hyprctl dispatch workspace 4
-
 # Editing package.json if necessary and launching the game
 jq 'if .name == "" then .name = "{}" else . end' "$game_dir/package.json" > /tmp/package.json && mv /tmp/package.json "$game_dir/package.json"
-$nwjs_dir/nw --nwapp="$game_dir" --ozone-platform-hint=wayland
+hyprctl dispatch -- exec "[float;center;workspace 4] $nwjs_dir/nw --nwapp='$game_dir'"

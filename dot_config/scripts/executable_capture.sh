7734b476f3dc999d977be7afd979d42b0da5cc4f
matsumiya8 <github@pyne.top>
2026-08-07 21:25:22 -0300

    more robust exec, supporting godot

--- dot_config/scripts/executable_capture.sh
+++ dot_config/scripts/executable_capture.sh
@@ -4,6 +4,7 @@ FILENAME="$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 7; echo)"
 killall -SIGINT -q gpu-screen-recorder && exit 0
 gpu-screen-recorder -w $1 -f 60 -k av1 -a default_output -o "/tmp/$FILENAME.mp4" && {
 	zenity --question --text="Upload?" || { rm -f "/tmp/$FILENAME.mp4"; exit; }
+    mkdir -p "$SS_FOLDER"
 	cp "/tmp/$FILENAME.mp4" "$SS_FOLDER" & sleep 0.5
 	rclone copy "/tmp/$FILENAME.mp4" pynes_stuff:uploads
 	if [ $? -eq 0 ]; then

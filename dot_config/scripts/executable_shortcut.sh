8f927f8b56366fdf7f8cb2196455d9fec65312d9
matsumiya8 <github@pyne.top>
2026-08-17 00:40:43 -0300

    b

--- dot_config/scripts/executable_shortcut.sh
+++ dot_config/scripts/executable_shortcut.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if an argument was provided
 if [ -z "$1" ]; then
     zenity --error --text="No executable file provided.\nUsage: $0 /path/to/game.exe"
     exit 1
@@ -8,42 +7,29 @@ fi
 
 EXE_PATH="$1"
 EXE_DIR=$(dirname "$EXE_PATH")
-ICON_OUTPUT_DIR="$HOME/.local/share/icons/exes/"
+ICON_OUTPUT_DIR="$HOME/.local/share/icons/exes"
+DESKTOP_DIR="$HOME/.local/share/applications/games"
 
-# Create icon directory if doesn't exist'
-mkdir -p "$ICON_OUTPUT_DIR"
+mkdir -p "$ICON_OUTPUT_DIR" "$DESKTOP_DIR"
 
 GAME_NAME=$(zenity --entry --title="Create Game Shortcut" --text="Enter the name of the game:" --entry-text="$(basename "${EXE_PATH%.*}")")
+[ -z "$GAME_NAME" ] && exit 0
 
-# Empty input check
-if [ -z "$GAME_NAME" ]; then
-    echo "Operation cancelled by user."
-    exit 0
-fi
-
-# Sanitize filename
 SAFE_NAME=$(echo "$GAME_NAME" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]//g')
-
-# Define paths
 ICON_PATH="$ICON_OUTPUT_DIR/ge_${SAFE_NAME}.png"
-DESKTOP_FILE="$HOME/.local/share/applications/ge_${SAFE_NAME}.desktop"
-
-# Create a temporary file for the raw icon
+DESKTOP_FILE="$DESKTOP_DIR/ge_${SAFE_NAME}.desktop"
 TEMP_ICO=$(mktemp --suffix=.ico)
-
-# Extract the first icon group found in the exe to the temp file
 icoextract "$EXE_PATH" "$TEMP_ICO" && sleep 1
 
-# Check if extraction resulted in a valid file
 if [ -s "$TEMP_ICO" ]; then
+    # Transparency fix & first item in the array is the largest icon usually
     convert "$TEMP_ICO[0]" -alpha on -background none -flatten "$ICON_PATH"
     rm "$TEMP_ICO"
 else
-    notify-send -t 2000 "Warning: No icon found in executable. Using generic icon."
+    notify-send -t 3000 "Warning: No icon found in executable. Using generic icon."
     ICON_PATH="applications-games" 
 fi
 
-# Create the .desktop file
 cat > "$DESKTOP_FILE" <<EOL
 [Desktop Entry]
 Type=Application
@@ -57,8 +43,5 @@ Categories=Game;
 StartupNotify=true
 EOL
 
-# Make it executable, notify and refresh the database
-chmod +x "$DESKTOP_FILE"
-[ -x "EXE_PATH" ] || chmod +x "$EXE_PATH"
-notify-send -e -t 2000 "Shortcut" "Shortcut created successfully!\n\nName: $GAME_NAME\nLocation: $DESKTOP_FILE"
-update-desktop-database "$HOME/.local/share/applications" &> /dev/null
+chmod +x "$DESKTOP_FILE" "$EXE_PATH"
+notify-send -e -t 3000 "Shortcut" "Shortcut created successfully!\n\nName: $GAME_NAME\nLocation: $DESKTOP_FILE"
